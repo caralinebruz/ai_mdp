d7e40c0c4c6ca193060e65ce53f752ae5e315192
caralinebruz <caraline.bruzinski@gmail.com>
2022-11-14 15:09:07 -0500

    fixing calculation

--- hw_file.py
+++ hw_file.py
@@ -2,49 +2,36 @@
 import math
 import random
 
-A = 0
-B = 0
-C = 0
-D = 0
-E = 0 
-F = 0 
-G = 0 
-H = 0 
-I = 0 
-J = 0 
-K = 0 
-L = 0 
-M = 0 
-N = 0 
-P = 0 
-Q = 0 
-T = 0
-
-A_Temp = 0
-B_Temp = 0
-C_Temp = 0
-D_Temp = 0
-E_Temp = 0 
-F_Temp = 0 
-G_Temp = 0 
-H_Temp = 0 
-I_Temp = 0 
-J_Temp = 0 
-K_Temp = 0 
-L_Temp = 0 
-M_Temp = 0 
-N_Temp = 0 
-P_Temp = 0 
-Q_Temp = 0 
-T_Temp = 0
-
+gamma = 0.85
 tolerance = 0.001
 # probability of failure
 # alpha = 0.15
 # p_success = 1 - alpha
 
+alphabet = [
+'A','B','C','D','E','F','G','H','I','J','K','L','M','N','P','Q'
+]
 
+Rewards = {
+	'A' : 11,
+	'B' : -1,
+	'C' : -1,
+	'D' : -1,
+	'E' : -1, 
+	'F' : -1, 
+	'G' : 4, 
+	'H' : -1, 
+	'I' : -1, 
+	'J' : 4, 
+	'K' : -1, 
+	'L' : -1, 
+	'M' : -1, 
+	'N' : -1, 
+	'P' : -1, 
+	'Q' : -11 
+}
 
+policy_hist = {}
 
 
 class MDP:
@@ -104,6 +91,10 @@ class MDP:
 				choice = random.choice(letters_neighbors)
 				print("initially setting policy for %s -> %s" % (letter, choice))
 				self.policy[letter] = choice
+
+		# for my logging and debugging, add it to the policy hist
+		for s,val in self.policy.items():
+			policy_hist[s] = [val]
 
 
 	def set_initial_values(self):
@@ -167,111 +158,7 @@ class MDP:
 
 
 			print(self.probabilities[letter])
-
-
-	def calculate_once(self):
-	
-		print("CALC SUBROUTINE")
-
-
-		### NOTE, i need to add convergence here, i dont do more than 1 iteration.
-
-
-		# make temp of the values 
-		pi_values = self.state_values.copy()
-
-		# compare what is the deal with these values initially
-		print("\n")
-		for k,v in pi_values.items():
-			print("%s:%s " % (k,v), end="")
-		print("\n")
-
-
-
-		for state in self.all_states:
-
-			print("working on state: %s" % state)
-
-			# make a backup copy of the value
-			temp = pi_values[state]
-			# for all of them, they are just assigned their iterative value
-			value = Rewards[state]
-
-			# dont need to do anything else for terminal 
-			if state in self.terminal_nodes:
-				pi_values[state] = value
-				print("\tnew value: %s" % value)
-
-
-
-			if state in self.decision_nodes:
-
-				to_node = self.policy[state]
-				print("\tshould go to node %s" % to_node)
-
-				restof_nodes = self.neighbors_directed[state].copy()
-				restof_nodes.remove(to_node)
-				# print("oringial resof+nodes %s" % restof_nodes)
-
-				# for the hw only
-				if self.self_loops:
-					if state in self.against_wall:
-						restof_nodes.append(state)
-
-				transition_states = []
-				transition_states.append(to_node)
-				transition_states.extend(restof_nodes)
-
-				print("\t other nodes: %s" % restof_nodes)
-
-				transition_likelihoods = self.probabilities[state]
-
-				if len(transition_likelihoods) != len(transition_states):
-					print("there is a huge problem here...")
-
-
-				for y in range(len(transition_states)):
-					transitional_value = self.state_values[transition_states[y]]
-					additive_value = gamma * transition_likelihoods[y] * transitional_value
-
-					value+= additive_value
-					pi_values[state] = value
-
-					print("\ty:%s, to_state:%s, transitional_value:%s, likelihood:%s == additive_value:%s" % (y, transition_states[y], transitional_value, transition_likelihoods[y], additive_value))
-					print("\tnew value: %s" % value)
-
-
-			if state in self.chance_nodes:
-
-				# J and G wont have self loops in the HW, so i wont write it now
-				# j and g are internal nodes without any self loops
-
-				restof_nodes = self.neighbors_directed[state].copy()
-
-				transition_states = restof_nodes
-				transition_likelihoods = self.probabilities[state]
-				if len(transition_likelihoods) != len(transition_states):
-					print("there is a huge problem here...")
-
-				for y in range(len(transition_states)):
-					transitional_value = self.state_values[transition_states[y]]
-					additive_value = gamma * transition_likelihoods[y] * transitional_value
-
-					value+= additive_value
-					pi_values[state] = value
-
-					print("\ty:%s, to_state:%s, transitional_value:%s, likelihood:%s == additive_value:%s" % (y, transition_states[y], transitional_value, transition_likelihoods[y], additive_value))
-					print("\tnew value: %s" % value)
-
-
-		# at the end, you'll ave the values for each node? what do i do with it?
-		for k,v in pi_values.items():
-			print("%s:%s " % (k,v), end="")
-		print("\n")
-
-		# at the end, update the overall state values with the iteration values??? or should i only do it after they converged?
-		self.state_values = pi_values
-				
+		
 
 
 	def _argmax(self, state):
@@ -280,22 +167,24 @@ class MDP:
 		next_policy = None
 
 		for adjacent_state in self.neighbors_directed[state]:
-			print("adj -> %s" % adjacent_state)
+			# print("adj -> %s" % adjacent_state)
 
 			if self.state_values[adjacent_state] > largest:
 
-				print("%s > %s" % (self.state_values[adjacent_state], largest))
+				# print("%s > %s" % (self.state_values[adjacent_state], largest))
 
 				largest = self.state_values[adjacent_state]
 				next_policy = adjacent_state
 			else:
-				print("not larger // %s vs %s" % (self.state_values[adjacent_state], largest))
+				pass
+				# print("not larger // %s vs %s" % (self.state_values[adjacent_state], largest))
 
 		return next_policy
 
 
 	def _value_iteration_inner(self, state_value_dict):
 
+		pi_current_values = state_value_dict.copy()
 		pi_state_values = {}
 
 		for state, curr_val in state_value_dict.items():
@@ -303,14 +192,18 @@ class MDP:
 
 		# for state in state_names:
 
-			print("inner %s" % state)
+			print("working on state %s" % state)
 			# for all of them, they are just assigned their iterative value
 			pi_value = Rewards[state]
+
+			print("\n")
 
 			# TERMINAL 
 			if state in self.terminal_nodes:
 				pi_state_values[state] = pi_value
-				print("value for terminal node %s" % pi_value)
+				# print("value for terminal node %s" % pi_value)
+
+				print("%s = %s" % (state,pi_value))
 
 
 			# CHANCE
@@ -325,26 +218,41 @@ class MDP:
 				if len(transition_likelihoods) != len(transition_states):
 					print("there is a huge problem here...")
 
+				# debugging
+				print("%s = %s " % (state,pi_value), end="")
+
 				for y in range(len(transition_states)):
+
+					adjacent_state_name = transition_states[y]
 
 					# this should be ok to pull from state_values 
 					# because they won't be updated until the convergence is finished
 					# transitional_value = self.state_values[transition_states[y]]
 
 					# ^ I DONT WANT THAT, you need the current iterative value
-					transitional_value = curr_val
+					# transitional_value = curr_val
+					# dont want that either, thats current value of the curent equation 
+					transitional_value = pi_current_values[adjacent_state_name]
+
+
 					additive_value = gamma * transition_likelihoods[y] * transitional_value
 
 					pi_value+= additive_value
 					pi_state_values[state] = pi_value
 
-					print("\ty:%s, to_state:%s, transitional_value:%s, likelihood:%s == additive_value:%s" % (y, transition_states[y], transitional_value, transition_likelihoods[y], additive_value))
-					print("\tnew value: %s" % pi_value)
+					# debugging
+					print("+ (%s)*(%s)*(%s) " % (gamma, transition_likelihoods[y], adjacent_state_name), end="")
+
+					# print("\ty:%s, to_state:%s, transitional_value:%s, likelihood:%s == additive_value:%s" % (y, transition_states[y], transitional_value, transition_likelihoods[y], additive_value))
+					# print("\tnew value: %s" % pi_value)
 
 			elif state in self.decision_nodes:
 
+				# debugging
+				print("%s = %s " % (state,pi_value), end="")
+
 				to_node = self.policy[state]
-				print("\tshould go to node %s" % to_node)
+				# print("\tshould go to node %s" % to_node)
 
 				restof_nodes = self.neighbors_directed[state].copy()
 				restof_nodes.remove(to_node)
@@ -358,7 +266,7 @@ class MDP:
 				transition_states.append(to_node)
 				transition_states.extend(restof_nodes)
 
-				print("\t other nodes: %s" % restof_nodes)
+				# print("\t other nodes: %s" % restof_nodes)
 
 				transition_likelihoods = self.probabilities[state]
 
@@ -371,14 +279,23 @@ class MDP:
 					# transitional_value = self.state_values[transition_states[y]]
 					# ^ dont want that, i want the current iterative value
 
-					transitional_value = curr_val
+					# transitional_value = curr_val
+					# dont want that either, thats current value of the curent equation 
+					adjacent_state_name = transition_states[y]
+					transitional_value = pi_current_values[adjacent_state_name]
+
 					additive_value = gamma * transition_likelihoods[y] * transitional_value
 
 					pi_value+= additive_value
 					pi_state_values[state] = pi_value
 
-					print("\ty:%s, to_state:%s, transitional_value:%s, likelihood:%s == additive_value:%s" % (y, transition_states[y], transitional_value, transition_likelihoods[y], additive_value))
-					print("\tnew value: %s" % pi_value)
+					# debugging
+					print("+ (%s)*(%s)*(%s) " % (gamma, transition_likelihoods[y], adjacent_state_name), end="")
+
+					# print("\ty:%s, to_state:%s, transitional_value:%s, likelihood:%s == additive_value:%s" % (y, transition_states[y], transitional_value, transition_likelihoods[y], additive_value))
+					# print("\tnew value: %s" % pi_value)
+
+			print("\n")
 
 
 		print(pi_state_values)
@@ -421,8 +338,12 @@ class MDP:
 
 			temp_pi_values = pi_values
 
+			if temp_pi_values == self.state_values:
+				print("yes, copy is first iteration")
+			else:
+				print("iterating with different pi_values")
 
-			# ret_vals = self._value_iteration_inner(pi_values.keys())
+
 			pi_values = self._value_iteration_inner(pi_values)
 
 
@@ -438,19 +359,13 @@ class MDP:
 
 			print("delta :%s \n" % delta)
 
-
-
-
 			if delta < tolerance:
 				print("I converge after %s iterations\n" % x)
 				converge = True
 
-
-		# make temp of the values 
-		# reset that pi_values should be the new state values
-		#return converge
-		# pi_values = self.state_values.copy()
-		
+			# # this is an experiment and i shouldnt keep this here
+			# print("running with SINGLE ITERATION ONLY as EXPERIMENT")
+			# converge=True
 
 
 		# compare what is the deal with these values initially
@@ -470,9 +385,6 @@ class MDP:
 
 
 		return converge
-
-
-
 
 
 
@@ -496,7 +408,6 @@ class MDP:
 				counter+=1
 
 			self.policy[state] = to_state
-
 			print("new policy ---> %s" % to_state)
 
 
@@ -505,7 +416,7 @@ class MDP:
 		print("new policy:")
 		for k,v in self.policy.items():
 			print("%s --> %s" % (k,v))
-
+			policy_hist[k].append(v)
 
 
 		return counter
@@ -524,6 +435,7 @@ class MDP:
 
 			y+=1
 			if y == 100:
+				print("BROKE OUT OF LOOP, limited y value, extend this??")
 				break
 
 			self.value_iteration()
@@ -553,155 +465,13 @@ while not converge
 	for s in S:
 		pi_star_s = argmax SUM(P*V(s'))
 '''
-# while not converge
 
 
-# def VI(A,B,C,D,E,F,G,H,I,J,K,L,M,N,P,Q):
-
-# 	print("A:%s, B:%s, C:%s, D:%s, E:%s, F:%s, G:%s, H:%s, I:%s, J:%s, K:%s, L:%s, M:%s, N:%s, P:%s, Q:%s" % (A,B,C,D,E,F,G,H,I,J,K,L,M,N,P,Q))
-# 	print()
-
-# 	VA = 3
-# 	VQ = 3
-# 	VB = -2 + .25*A + .25*F + .25*C + .25*B
-# 	VC = -2 + .25*B + .25*G + .25*D + .25*C
-# 	VD = -2 + .333*C + .333*D + .333*H
-# 	VE = -2 + .25*A + .25*F + .25*I + .25*E
-# 	VF = -2 + .25*E + .25*B + .25*G + .25*J
-# 	VG = -2 + .25*F + .25*C + .25*H + .25*K
-# 	VH = -2 + .25*D + .25*L + .25*G + .25*H
-# 	VI = -2 + .25*E + .25*J + .25*M + .25*I
-# 	VJ = -2 + .25*I + .25*F + .25*K + .25*N
-# 	VK = -2 + .25*J + .25*G + .25*L + .25*P
-# 	VL = -2 + .25*K + .25*H + .25*Q + .25*L
-# 	VM = -2 + .333*I + .333*N + .333*M
-# 	VN = -2 + .25*M + .25*J + .25*P + .25*N 
-# 	VP =  -2 + .25*N + .25*K + .25*Q + .25*P
-
-# 	return VA, VB, VC, VD, VE, VF, VG, VH, VI, VJ, VK, VL, VM, VN, VP, VQ
-
-
-alphabet = [
-'A','B','C','D','E','F','G','H','I','J','K','L','M','N','P','Q'
-]
-
-# temps = [
-# A_Temp, B_Temp, C_Temp, D_Temp, E_Temp, F_Temp, G_Temp, H_Temp, I_Temp, J_Temp, K_Temp, L_Temp, M_Temp, N_Temp, P_Temp, Q_Temp
-# ]
-
-
-
-Rewards = {
-	'A' : 11,
-	'B' : -1,
-	'C' : -1,
-	'D' : -1,
-	'E' : -1, 
-	'F' : -1, 
-	'G' : 4, 
-	'H' : -1, 
-	'I' : -1, 
-	'J' : 4, 
-	'K' : -1, 
-	'L' : -1, 
-	'M' : -1, 
-	'N' : -1, 
-	'P' : -1, 
-	'Q' : -11 
-}
-
-
-
-
-# def value_iteration_to_convergence(A,B,C,D,E,F,G,H,I,J,K,L,M,N,P,Q):
-# 	# set initial loop vars
-# 	converge = False
-# 	x = 0
-
-# 	while not converge:
-
-# 		print("\nIteration %s:" % x)
-
-# 		if x == 1000:
-# 			break
-
-# 		delta = 0
-
-		# A_Temp = A
-		# B_Temp = B
-		# C_Temp = C
-		# D_Temp = D
-		# E_Temp = E
-		# F_Temp = F
-		# G_Temp = G
-		# H_Temp = H
-		# I_Temp = I
-		# J_Temp = J
-		# K_Temp = K
-		# L_Temp = L
-		# M_Temp = M
-		# N_Temp = N
-		# P_Temp = P
-		# Q_Temp = Q
-
-		# A,B,C,D,E,F,G,H,I,J,K,L,M,N,P,Q = VI(A,B,C,D,E,F,G,H,I,J,K,L,M,N,P,Q)
-
-		# delta = max(delta,( abs(A_Temp - A) ))
-		# print("delta %s" % delta, end="")
-		# delta = max(delta,( abs(B_Temp - B) ))
-		# print(" delta %s" % delta, end="")
-		# # delta = max(delta,( abs(C_Temp - C) ))
-		# print(" delta %s" % delta, end="")
-
-		# # 38.10093457943922
-		# delta = max(delta,( abs(D_Temp - D) ))
-		# print(" D_delta: %s" % delta, end="")
-
-		# delta = max(delta,( abs(E_Temp - E) ))
-		# print(" delta %s" % delta, end="")
-
-		# delta = max(delta,( abs(F_Temp - F) ))
-		# print(" delta %s" % delta, end="")
-		# delta = max(delta,( abs(G_Temp - G) ))
-		# print(" delta %s" % delta, end="")
-		# delta = max(delta,( abs(H_Temp - H) ))
-		# print(" delta %s" % delta, end="")
-		# delta = max(delta,( abs(I_Temp - I) ))
-		# print(" delta %s" % delta, end="")
-
-		# delta = max(delta,( abs(J_Temp - J) ))
-	# 	print(" delta %s" % delta, end="")
-	# 	delta = max(delta,( abs(K_Temp - K) ))
-	# 	print(" delta %s" % delta, end="")
-	# 	delta = max(delta,( abs(L_Temp - L) ))
-	# 	print(" delta %s" % delta, end="")
-	# 	delta = max(delta,( abs(M_Temp - M) ))
-	# 	print(" delta %s" % delta, end="")
-	# 	delta = max(delta,( abs(N_Temp - N) ))
-	# 	print(" delta %s" % delta, end="")
-	# 	delta = max(delta,( abs(P_Temp - P) ))
-	# 	print(" delta %s" % delta, end="")
-	# 	delta = max(delta,( abs(Q_Temp - Q) ))
-	# 	print(" delta %s" % delta, end="")
-
-	# 	print("delta %s" % delta)
-
-
-	# 	if delta < tolerance:
-	# 		print("I converge after %s iterations\n" % x)
-	# 		converge = True
-
-	# 	x+=1
-
-	# return A,B,C,D,E,F,G,H,I,J,K,L,M,N,P,Q
-
-
-
-gamma = 0.85
 
 if __name__ == '__main__':
 
 	M = MDP()
+
 	# set initial policy is random
 	M.make_random_policy()
 	M.set_initial_values()
@@ -710,39 +480,18 @@ if __name__ == '__main__':
 	# make the equations
 	M.set_up_probabilities()
 
-	# M.calculate_once()
-	# M.recalculate_policy()
-
-	# M.value_iteration()
-	# policy_change = M.recalculate_policy()
-
 	M.policy_iteration()
 
 
 
 
 
+	for k,v in policy_hist.items():
+		print("%s : %s" % (k,v))
 
 
-
-
-
-
-
-
-
-
-	# A,B,C,D,E,F,G,H,I,J,K,L,M,N,P,Q = value_iteration_to_convergence(A,B,C,D,E,F,G,H,I,J,K,L,M,N,P,Q)
-
-
-
-
-
-
-
-
-
-
+	for state, final_val in M.state_values.items():
+		print("%s -> %s" % (state, final_val))
 
 
 
