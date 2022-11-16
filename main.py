aa5befb0a90d687d01d05bb985aceb1b83afe857
caralinebruz <caraline.bruzinski@gmail.com>
2022-11-16 13:59:28 -0500

    adding arg parse

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
+import os
+import sys
+import getopt
+import argparse
 
-
+import parse
 
 
 
@@ -21,30 +25,39 @@ if __name__ == '__main__':
 	# USAGE: ./main.py -v 2 data/input/tiny.txt
 	# mdp -df .9 -tol 0.0001 some-input.txt
 
+	# ./main.py -df .9 -tol 0.0001 data/input/maze.txt
+
 
 	#
 	# PARSE COMMAND LINE 
 	#
 
-	infile = "" # graph file
+	infile = "" # file
+	discount_factor = 1	#default
+	tolerance = 0.001
 	lines = []
 
-	parser = argparse.ArgumentParser(description='Colormap parser')
-	parser.add_argument('-v', action='store_true', help="verbose flag")
-	parser.add_argument('num_colors',type=int, nargs=1)
-	parser.add_argument('graph_file', type=argparse.FileType('r'))
+
+	parser = argparse.ArgumentParser(description='MDP parser')
+	parser.add_argument('-df', help="discount factor")
+	parser.add_argument('-tol', help="tolerance")
+	parser.add_argument('input_file', type=argparse.FileType('r'))
 
 	args = parser.parse_args()
-	# print(args)
+	#print(args)
 
-	if args.v:
-		v_verbose = True
-	if args.num_colors:
-		num_colors = int(args.num_colors[0])
-	if args.graph_file:
-		lines = args.graph_file.readlines()
-		infile = args.graph_file
+	if not args.df:
+		print("You didn't input a discount factor. Using default of 1")
+	else:
+		discount_factor = args.df
+	if not args.tol:
+		print("You didn't input a tolerance. Using default of 0.001")
+	else:
+		tolerance = args.tol
+	if args.input_file:
+		lines = args.input_file.readlines()
+		infile = args.input_file
 
+	print("discount_factor:%s, tolerance:%s" % (discount_factor, tolerance))
 
-	map_coloring_via_dpll(infile, lines)
 
