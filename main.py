aea54c2580ac2d04bf823832b9d0a5cc1b1e5ea7
caralinebruz <caraline.bruzinski@gmail.com>
2022-11-16 15:15:23 -0500

    adding initial parsing code for raw file

--- main.py
+++ main.py
@@ -5,10 +5,31 @@ import getopt
 import argparse
 
 import parse
+from parse import Parser
+
+from pprint import pprint
 
 
 
 
+def determine_solver_method(lines):
+
+	pass
+
+
+
+def main(discount_factor, tolerance, infile, lines):
+
+	# parse the file into necessary pieces
+	# detect cycles (if any)
+	# determine solver method (mdp vs etc....)
+
+
+	p = Parser(lines)
+	props = p.parse_main()
+
+
+	method = determine_solver_method(lines)
 
 
 
@@ -22,21 +43,16 @@ import parse
 
 
 if __name__ == '__main__':
-	# USAGE: ./main.py -v 2 data/input/tiny.txt
-	# mdp -df .9 -tol 0.0001 some-input.txt
-
-	# ./main.py -df .9 -tol 0.0001 data/input/maze.txt
-
+	# USAGE: 
+	# 	./main.py -df .9 -tol 0.0001 data/input/maze.txt
 
 	#
 	# PARSE COMMAND LINE 
 	#
-
 	infile = "" # file
 	discount_factor = 1	#default
 	tolerance = 0.001
 	lines = []
-
 
 	parser = argparse.ArgumentParser(description='MDP parser')
 	parser.add_argument('-df', help="discount factor")
@@ -59,5 +75,7 @@ if __name__ == '__main__':
 		infile = args.input_file
 
 	print("discount_factor:%s, tolerance:%s" % (discount_factor, tolerance))
+
+	main(discount_factor, tolerance, infile, lines)
 
 
---
+++ parse.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python3
+import sys
+from pprint import pprint
+
+
+
+class Parser:
+
+	def __init__(self, raw_lines):
+		self.nodes_list = []
+		self.node_adjacency_mappings = {}
+		self.rewards = {}
+		self.probabilities = {}
+		self.lines = [line.rstrip('\n') for line in raw_lines]
+
+		self.chance_nodes = []
+		self.decision_nodes = []
+		self.terminal_nodes = []
+
+
+	def raw_parse(self):
+		''' Go through the file initially
+				save names and values of things etc.
+		'''
+		for line in self.lines:
+
+			# is a directed mapping parent > child
+			if ":" in line:
+				data = line.split(':')
+
+				# save node name
+				node_name = data[0].strip(' ')
+				if node_name not in self.nodes_list:
+					self.nodes_list.append(node_name)
+
+				# add the children of this to the node_adjacency_mappings
+				children = data[1].lstrip()
+				children = children.replace(' ','')
+				children = children.strip('[] ').split(',')
+
+				# true if all the strings are non-empty
+				if all(children):
+					self.node_adjacency_mappings[node_name] = children
+					for i in children:
+						if i not in self.nodes_list:
+							self.nodes_list.append(i)
+
+			# is a probability
+			elif "%" in line:
+				data = line.split('%')
+
+				# save node name
+				node_name = data[0].strip(' ')
+				if node_name not in self.nodes_list:
+					self.nodes_list.append(node_name)
+
+				# add the probabilities
+				probs = data[1].lstrip()
+				probs = probs.replace(' ','')
+				probs = probs.strip('[] ').split(',')
+
+				# true if all the strings are non-empty
+				if all(probs):
+					self.probabilities[node_name] = probs
+
+			# is a reward
+			elif "=" in line:
+				data = line.split('=')
+
+				# save node name
+				node_name = data[0].strip(' ')
+				if node_name not in self.nodes_list:
+					self.nodes_list.append(node_name)
+
+				# add reward to dict
+				self.rewards[node_name] = data[1]
+
+		print("names of all nodes in the graph:")
+		print(self.nodes_list)
+		print("node-adjacency-mappings:")
+		pprint(self.node_adjacency_mappings)
+		print("probabilities:")
+		pprint(self.probabilities)
+		print("rewards:")
+		pprint(self.rewards)
+
+
+	def clean_data(self):
+		'''Go through data check valid things
+
+		1. If a node has edges but no probability entry
+		2. If a node has edges but no reward entry
+		3. If a node has no edges it is terminal
+		4. If a node has a single edge it always transitions there
+		5. A node referenced as an edge must one of the three entries
+		'''
+		for state, edges in self.node_adjacency_mappings.items():
+
+			# case 1
+			if state not in self.probabilities.keys():
+				self.decision_nodes.append(state)
+				self.probabilities[state] = [1]
+
+			# case 2
+			if state not in self.rewards.keys():
+				self.rewards[state] = 0
+
+
+			# case 4
+			if len(edges) == 1:
+				# it will always transition to that edge with p=1
+				if state not in self.probabilities.keys():
+					self.probabilities[state] = [1]
+
+		# case 3
+		for state in self.nodes_list:
+			if state not in self.node_adjacency_mappings.keys():
+				# it is terminal
+				self.terminal_nodes.append(state)
+				# consider error
+				if state in self.probabilities.keys():
+					print("case 3: error")
+					print("removing probability entry for terminal node")
+					del self.probabilities[state]
+
+		# case 5
+		for parent_state, edges in self.node_adjacency_mappings.items():
+			# must have reward, probability, or edges
+
+			for child_state in edges:
+				valid = False
+				if child_state in self.probabilities.keys():
+					valid = True
+				if child_state in self.rewards.keys():
+					valid = True
+				if child_state in self.node_adjacency_mappings.keys():
+					valid = True
+
+			if not valid:
+				print("case 5: die because something that was listed as an edge doesnt meet one of three criteria")
+				sys.exit(1)
+
+
+
+	def enrich_data(self):
+		# figure out which nodes are decision nodes
+		for state, probs in self.probabilities.items():
+
+			# decision node
+			if len(probs) == 1:
+				if state not in self.decision_nodes:
+					self.decision_nodes.append(state)
+
+			# chance node
+			elif len(probs) > 1:
+				if state not in self.chance_nodes:
+					self.chance_nodes.append(state)
+
+
+		print("chance nodes")
+		print(self.chance_nodes)
+
+		print("decision nodes")
+		print(self.decision_nodes)
+
+		print("terminal nodes")
+		print(self.terminal_nodes)
+
+
+
+	def parse_main(self):
+
+		# go through the file initially and collect properties
+		self.raw_parse()
+
+		# error checking on inputs
+		self.clean_data()
+
+		# go through collected properties and enrich the information
+		self.enrich_data()
+
+
+		# check if the data is cyclic or acyclic
+
+
+
+
+
+
+
+
+
